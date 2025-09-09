0fe6355f314eede623322fe4121a706e5b74eb03
RubioJulian <jarubioavila@gmail.com>
2025-09-09 17:30:11 -0500

    Add files via upload

---
+++ Corte_1/Calculator.java
@@ -0,0 +1,41 @@
+
+package javaapplication;
+import java.util.Scanner;
+
+public class Calculator {
+    public Calculator(){
+     
+    }
+        public int add(int D1, int D2){
+            return D1+D2;
+        }
+        public int substract(int D1, int D2){
+            return D1-D2;
+        
+        }
+        public int multiply(int D1, int D2){
+            return D1*D2;
+        }
+        public double divide(int D1, int D2){
+            return D1/D2;
+        }
+         public int modulo(int D1, int D2){
+             return D1%D2;
+         }
+                public static void main(String[]args)
+                {
+                    Scanner entrada = new Scanner(System.in);
+                    System.out.print("Escriba la primera variable ");
+                    int D1 = entrada.nextInt();
+                    System.out.print("Escriba la segunda variable ");
+                    int D2 = entrada.nextInt();
+              
+                    
+                    Calculator mycalculator = new Calculator();
+                    System.out.println(mycalculator.add(D1,D2));
+                    System.out.println(mycalculator.substract(D1,D2));
+                    System.out.println(mycalculator.multiply(D1,D2));
+                }
+    }
+    
+
